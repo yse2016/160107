ffe0421c4c99dc7cca49dd05e00c9be37e25b0af
MiyataMasashi <miyata_masashi@yse-c.net>
2016-12-13 15:07:19 +0900

    aloha.java updated

--- 20161213/aloha.java
+++ 20161213/aloha.java
@@ -1,6 +1,9 @@
 public class aloha {
   public static void main( String[] main){
   	for( int i = 0; i < 20; i++ ){
+	 if( i%3 == 0 ){
+	  System.out.println("アォハ");
+	 } else
 	  System.out.println("ALOHA!");
 	}
   }
