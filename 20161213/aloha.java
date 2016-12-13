ee1f8ed7db5f79f4aa5499f68400713d9e396d0e
MiyataMasashi <miyata_masashi@yse-c.net>
2016-12-13 15:28:42 +0900

    aloha.java updated

--- 20161213/aloha.java
+++ 20161213/aloha.java
@@ -1,10 +1,15 @@
 public class aloha {
-  public static void main( String[] main){
-  	for( int i = 0; i < 20; i++ ){
-	 if( i%3 == 0 ){
-	  System.out.println("アォハ");
-	 } else
-	  System.out.println("ALOHA!");
-	}
+  public static void main( String[] args){
+	String mainMsg;
+	String subMsg;
+	while( true ) {
+		System.out.println("come on");
+		mainMsg = new java.util.Scanner(System.in).nextLine();
+		if ( mainMsg.equals("exit") ) {
+			break;
+		} else {
+  			System.out.println(mainMsg);
+  		}
+  	}
   }
 }
