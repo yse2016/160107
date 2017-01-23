7e1190268c4574fe48c7724a2139b4455629e5b2
宮田 真志 <160107@yses.ac.jp>
2017-01-23 11:51:32 +0900

    button done

--- Idean/Idean.java
+++ Idean/Idean.java
@@ -9,8 +9,12 @@ public class Idean {
 	}
 }
 
-class IdeaMan {
+class IdeaMan implements ActionListener {
 	JFrame frame;
+	JButton btn1;
+	JButton btn2;
+	JPanel panel;
+	JTextField field;
 
 	public IdeaMan() {
 		frame = new JFrame();
@@ -19,6 +23,23 @@ class IdeaMan {
 		frame.setLocationRelativeTo( null );
 		frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
 
+		field = new JTextField( 20 );
+		panel = new JPanel();
+		panel.add( field );
+		Container con = frame.getContentPane();
+		con.setLayout( new GridLayout( 2, 1 ) );
+		con.add( panel );
+
+		btn1 = new JButton(	"表示" );
+		btn2 = new JButton( "保存" );
+
+		panel.add( btn1 );
+		panel.add( btn2 );
+
 		frame.setVisible( true );
+	}
+
+	public void actionPerformed(ActionEvent ae) { 
+
 	}
 }
